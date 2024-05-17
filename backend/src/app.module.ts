93cadeefa096638a3b473af8f722daf241fd44b9
JoyalPeter <joyalpeterhere@gmail.com>
2024-05-17 22:53:11 +0530

    Database integration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './resources/users/users.module';
+import { ProjectsModule } from './resources/projects/projects.module';
+import { TodosModule } from './resources/todos/todos.module';
+import { User } from './resources/users/entities/user.entity';
+import { Project } from './resources/projects/entities/project.entity';
+import { Todo } from './resources/todos/entities/todo.entity';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      database: process.env.DATABASE_NAME,
+      password: process.env.DATABASE_PASSWORD,
+      entities: [User, Project, Todo],
+      synchronize: true,
+    }),
+    UsersModule,
+    ProjectsModule,
+    TodosModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './utils/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
