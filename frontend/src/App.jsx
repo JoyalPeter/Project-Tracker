dd8f12d177b9eeff332f628121296b0672d4c69b
JoyalPeter <joyalpeterhere@gmail.com>
2024-05-18 00:31:32 +0530

    Lazy loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-import SignIn from "./pages/SignIn/SignIn";
-import SignUp from "./pages/SignUp/SignUp";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
 import Toaster from "./components/Toaster/Toaster";
 import PrivateRoutes from "./utils/PrivateRoutes";
-import HomePage from "./pages/HomePage/HomePage";
-import SearchPage from "./pages/SearchPage/SearchPage";
-import ProjectPage from "./pages/ProjectPage/ProjectPage";
-import AllProjectsPage from "./pages/AllProjectsPage/AllProjectsPage";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Loader from "./components/Loader/Loader";
+
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
+const SignIn = lazy(() => import("./pages/SignIn/SignIn"));
+const SignUp = lazy(() => import("./pages/SignUp/SignUp"));
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const SearchPage = lazy(() => import("./pages/SearchPage/SearchPage"));
+const ProjectPage = lazy(() => import("./pages/ProjectPage/ProjectPage"));
+const AllProjectsPage = lazy(() =>
+  import("./pages/AllProjectsPage/AllProjectsPage")
+);
 
 const darkTheme = createTheme({
   palette: {
@@ -21,17 +26,19 @@ function App() {
     <>
       <ThemeProvider theme={darkTheme}>
         <BrowserRouter>
-          <Routes>
-            <Route path="/signin" Component={SignIn} />
-            <Route path="/signup" Component={SignUp} />
-            <Route element={<PrivateRoutes />}>
-              <Route path="/" Component={HomePage} />
-              <Route path="/projects/all" Component={AllProjectsPage} />
-              <Route path="/projects/:projectID" Component={ProjectPage} />
-              <Route path="/projects/search" Component={SearchPage} />
-            </Route>
-            <Route path="*" Component={NotFoundPage} />
-          </Routes>
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/signin" Component={SignIn} />
+              <Route path="/signup" Component={SignUp} />
+              <Route element={<PrivateRoutes />}>
+                <Route path="/" Component={HomePage} />
+                <Route path="/projects/all" Component={AllProjectsPage} />
+                <Route path="/projects/:projectID" Component={ProjectPage} />
+                <Route path="/projects/search" Component={SearchPage} />
+              </Route>
+              <Route path="*" Component={NotFoundPage} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
         <Toaster />
       </ThemeProvider>
