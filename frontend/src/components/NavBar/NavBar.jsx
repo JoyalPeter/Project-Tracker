554948c75d6a0f6dc2ef84b4b7be2f388bc1600b
JoyalPeter <joyalpeterhere@gmail.com>
2024-05-21 10:32:57 +0530

    User avatar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,8 +1,10 @@
 import Box from "@mui/material/Box";
+import Menu from "@mui/material/Menu";
 import AppBar from "@mui/material/AppBar";
 import Button from "@mui/material/Button";
 import Toolbar from "@mui/material/Toolbar";
 import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
 import HomeIcon from "@mui/icons-material/Home";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
@@ -15,12 +17,24 @@ import { useTheme } from "@mui/material/styles";
 import { ColorModeContext } from "../../utils/ColorTheme";
 
 import Logout from "../Logout/Logout";
+import { Avatar } from "@mui/material";
 
 export default function NavBar() {
   const navigate = useNavigate();
   const theme = useTheme();
   const colorMode = useContext(ColorModeContext);
   const [openLogout, setOpenLogout] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const openMenu = Boolean(anchorEl);
+
+  function handleClick(event){
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
+
   return (
     <>
       <Box sx={{ flexGrow: 1, marginBottom: "2%" }}>
@@ -57,19 +71,34 @@ export default function NavBar() {
                 )}
               </IconButton>
             </Tooltip>
-            <Button
-              color="inherit"
-              data-testid="test-navbar-logout"
-              startIcon={<LogoutIcon />}
-              onClick={() => {
-                setOpenLogout(true);
-              }}
-            >
-              Logout
-            </Button>
+            <IconButton onClick={handleClick}>
+              <Avatar>{localStorage.getItem("username")[0]}</Avatar>
+            </IconButton>
           </Toolbar>
         </AppBar>
       </Box>
+      <Menu
+        anchorEl={anchorEl}
+        open={openMenu}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem>
+          <Button
+            color="inherit"
+            data-testid="test-navbar-logout"
+            startIcon={<LogoutIcon />}
+            onClick={() => {
+              handleClose()
+              setOpenLogout(true);
+            }}
+          >
+            Logout
+          </Button>
+        </MenuItem>
+      </Menu>
       <Logout open={openLogout} setOpen={setOpenLogout} />
     </>
   );
